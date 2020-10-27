f161ea7e73150cd5c1310167fdcbec4c26df140f
Zeke Dellavechia <67669609+zekedellavechia@users.noreply.github.com>
2020-10-27 11:19:41 -0300

    Update transaccionsTest.py

--- transaccionsTest.py
+++ transaccionsTest.py
@@ -2,13 +2,13 @@ from test import *
 testBase = TestBase()
 print(testBase.driver.session_id)
 
-itemIds = ["010050782684", "010050761557", "010050763993", "234569999999", "456789999999", "345679999999"]
+itemIds = ["111111111", "22222222222", "33333333", "4444444444", "55555555555", "66666666"]
 i = 0
 random_bulk = random.randint(1, 4)
 random_int = round(random.uniform(1, 999))
 random_float = round(random.uniform(1, 999), 2)
 try:
-    testBase.login("300011113", "ezcorp")
+    testBase.login("111111111111", "xxxxxxxxxxx")
     while i < 6:
         testBase.add_bulk_item(random_bulk, random_int, random_float)
         testBase.start_checkout()
